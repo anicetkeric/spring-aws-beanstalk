ef08f69485c3d1f8ff3e4f95b22947cc8b0b2138
aek <anicetkeric@outlook.com>
2023-09-25 19:35:20 +0200

    initial architecture

---
+++ src/main/java/com/example/awsbeanstalk/SpringAwsBeanstalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsbeanstalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsBeanstalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsBeanstalkApplication.class, args);
+	}
+
+}
